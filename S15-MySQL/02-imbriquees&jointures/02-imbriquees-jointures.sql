7ec6684305032521570827315e5901ebc09fef32
Pierral87 <pa.lacaze1@gmail.com>
2025-05-20 11:55:11 +0200

    MySQL Chapter 2 InProgress

---
+++ S15-MySQL/02-imbriquees&jointures/02-imbriquees-jointures.sql
@@ -0,0 +1,99 @@
+CREATE DATABASE bibliotheque;
+USE bibliotheque;
+
+CREATE TABLE abonne (
+  id_abonne INT NOT NULL AUTO_INCREMENT,
+  prenom VARCHAR(25) NOT NULL,
+  PRIMARY KEY (id_abonne)
+) ENGINE=InnoDB ;
+
+INSERT INTO abonne (id_abonne, prenom) VALUES
+(1, 'Guillaume'),
+(2, 'Benoit'),
+(3, 'Chloe'),
+(4, 'Laura');
+
+
+CREATE TABLE livre (
+  id_livre INT NOT NULL AUTO_INCREMENT,
+  auteur VARCHAR(25) NOT NULL,
+  titre VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id_livre)
+) ENGINE=InnoDB ;
+
+INSERT INTO livre (id_livre, auteur, titre) VALUES
+(100, 'GUY DE MAUPASSANT', 'Une vie'),
+(101, 'GUY DE MAUPASSANT', 'Bel-Ami '),
+(102, 'HONORE DE BALZAC', 'Le pere Goriot'),
+(103, 'ALPHONSE DAUDET', 'Le Petit chose'),
+(104, 'ALEXANDRE DUMAS', 'La Reine Margot'),
+(105, 'ALEXANDRE DUMAS', 'Les Trois Mousquetaires');
+
+CREATE TABLE emprunt (
+  id_emprunt INT NOT NULL AUTO_INCREMENT,
+  id_livre INT DEFAULT NULL,
+  id_abonne INT DEFAULT NULL,
+  date_sortie DATE NOT NULL,
+  date_rendu DATE DEFAULT NULL,
+  PRIMARY KEY  (id_emprunt)
+) ENGINE=InnoDB ;
+
+INSERT INTO emprunt (id_emprunt, id_livre, id_abonne, date_sortie, date_rendu) VALUES
+(1, 100, 1, '2016-12-07', '2016-12-11'),
+(2, 101, 2, '2016-12-07', '2016-12-18'),
+(3, 100, 3, '2016-12-11', '2016-12-19'),
+(4, 103, 4, '2016-12-12', '2016-12-22'),
+(5, 104, 1, '2016-12-15', '2016-12-30'),
+(6, 105, 2, '2017-01-02', '2017-01-15'),
+(7, 105, 3, '2017-02-15', NULL),
+(8, 100, 2, '2017-02-20', NULL);
+
+-- QUels sont les id_livre des livres qui n'ont pas été rendu à la bibliothèque ? 
+SELECT id_livre FROM emprunt WHERE date_rendu IS NULL;
++----------+
+| id_livre |
++----------+
+|      105 |
+|      100 |
++----------+
+-- Attention, la valeur NULL se teste avec IS ou IS NOT 
+
+-- Pour avoir les titres de ces livres, je dois réussir à aller piocher l'information dans la table livre (alors que l'information "non rendu" se trouve dans la table emprunt)
+-- 2 possibilités :
+    -- Requête imbriquée
+    -- Jointure
+
+
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+----------- REQUETES IMBRIQUEES (sur plusieurs tables) ---------------------------------
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+-- Quels sont les titres des livres qui n'ont pas été rendu à la bibliothèque ?
+
+-- Quel est le titre du livre id 105 ?
+SELECT titre FROM livre WHERE id_livre = 105;
+
+-- Quels sont les titres des livres id 105 et 100 ?
+SELECT titre FROM livre WHERE id_livre IN (105,100);
+
+-- Quelle est la requête qui me permet de récupérer les id 105 et 100 ?
+SELECT id_livre FROM emprunt WHERE date_rendu IS NULL;
+
+-- Donc, la requête imbriquée serait ? 
+-- Quels sont les titres des livres qui n'ont pas été rendu à la bibliothèque ?
+SELECT titre FROM livre WHERE id_livre IN 
+    (SELECT id_livre FROM emprunt WHERE date_rendu IS NULL);
+-- Les requêtes imbriquées s'executent, si on voulait l'imaginer de façon logique, par la fin : J'ai besoin du résultat de la requête de second niveau pour mener à bien la requête de premier niveau 
+-- Il me faut forcément un champ commun ! Ici la primary key de livre (id_livre) grâce a sa relation avec emprunt, existe en tant que foreign key dans la table emprunt, ce qui me permet de chercher une condition dans la table emprunt, tout en affichant une information venant de la table livre
+-- Dans ce cas précis, je suis obligé d'utiliser IN car la sous requête me renvoie plus d'un résultat, un "=" ne fonctionnera pas 
+
+-- EXERCICE 1 : Quels sont les prénoms des abonnés n'ayant pas rendu un livre à la bibliotheque.
+-- EXERCICE 2 : Nous aimerions connaitre le(s) n° des livres empruntés par Chloé
+-- EXERCICE 3 : Affichez les prénoms des abonnés ayant emprunté un livre le 07/12/2016.
+-- EXERCICE 4 : combien de livre Guillaume a emprunté à la bibliotheque ?
+-- EXERCICE 5 : Affichez la liste des abonnés ayant déjà emprunté un livre d'Alphonse Daudet
+-- EXERCICE 6 : Nous aimerions connaitre les titres des livres que Chloe a emprunté à la bibliotheque.
+-- EXERCICE 7 : Nous aimerions connaitre les titres des livres que Chloe n'a pas emprunté à la bibliotheque.
+-- EXERCICE 8 : Nous aimerions connaitre les titres des livres que Chloe a emprunté à la bibliotheque ET qui n'ont pas été rendu.
+-- EXERCICE 9 : Qui a emprunté le plus de livre à la bibliotheque ?
