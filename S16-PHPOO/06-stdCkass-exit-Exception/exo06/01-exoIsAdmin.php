abbaeb75aeb1cfd57a87952542c10672d2393113
Pierral87 <pa.lacaze1@gmail.com>
2025-06-25 13:41:20 +0200

    PHP OO Chapitre 6 Exo Start

---
+++ S16-PHPOO/06-stdCkass-exit-Exception/exo06/01-exoIsAdmin.php
@@ -0,0 +1,12 @@
+<?php 
+
+/* 
+
+    Exercice : Restreindre l'accès à une page uniquement un rôle prévu, le rôle admin 
+
+    Enoncé : 
+        Simulez un utilisateur connecté dans la session, prévoir un indice qui contiendra les informations de l'utilisateur notamment son rôle 
+        Si l'utilisateur n'a pas le rôle admin, empéchez le d'accèder à cette page avec un message d'erreur à la place du contenu de la page
+
+
+*/
